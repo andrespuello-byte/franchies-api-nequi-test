7df6cd164cbb2da71e537c9b6c92252d3b54dbbb
Andres Puello Osorio <andres.puello@pragma.com.com>
2026-01-26 13:58:31 -0500

    feat: add ignore terraform files and folder

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,7 +3,17 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "vpc_id" {
+  description = "VPC donde se despliega la infraestructura"
+  type        = string
+}
+
 variable "project_name" {
   type    = string
   default = "franchises-api"
+}
+
+variable "public_subnets" {
+  description = "Subnets públicas para el ALB"
+  type        = list(string)
 }
