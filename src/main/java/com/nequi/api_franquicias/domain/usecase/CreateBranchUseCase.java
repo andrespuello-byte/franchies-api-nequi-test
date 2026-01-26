86f755632fd0a7b3efa74da4ba5cd710c5e2e979
Andres Puello Osorio <andres.puello@pragma.com.com>
2026-01-26 14:26:28 -0500

    refactor: improve CreateBranchUseCase with name validation

--- src/main/java/com/nequi/api_franquicias/domain/usecase/CreateBranchUseCase.java
+++ src/main/java/com/nequi/api_franquicias/domain/usecase/CreateBranchUseCase.java
@@ -19,24 +19,34 @@ public class CreateBranchUseCase {
     }
 
     public Mono<Franchise> execute(String name, String franchiseId) {
-        return persistencePort
-                .findById(franchiseId)
-                .switchIfEmpty(Mono.error(new BussinesException(ErrorMessage.FRANCHISE_NOT_FOUND)))
-                .flatMap(franchise -> {
-                    Branch newBranch = Branch.builder()
-                            .id(generateId())
-                            .name(name.trim())
-                            .build();
-
-                    List<Branch> updatedBranches = new ArrayList<>(franchise.getBranches());
-                    updatedBranches.add(newBranch);
-
-                    franchise.setBranches(updatedBranches);
-                    return persistencePort.save(franchise);
-                });
+        if(isInvalidName(name))
+            return Mono.error(new BussinesException(ErrorMessage.BRANCH_NAME_INVALID));
+        return findFranchiseById(franchiseId)
+                .flatMap(franchise -> addBranchToFranchise(franchise, name))
+                .flatMap(persistencePort::save);
     }
 
-    public boolean isEmptyName(String name){
+    public boolean isInvalidName(String name){
         return name.isEmpty();
+    }
+
+    private Mono<Franchise> findFranchiseById(String franchiseId){
+        return persistencePort
+                .findById(franchiseId)
+                .switchIfEmpty(Mono.error(new BussinesException(ErrorMessage.FRANCHISE_NOT_FOUND)));
+    }
+
+    private Mono<Franchise> addBranchToFranchise(Franchise franchise, String name){
+        List<Branch> updateBranches = new ArrayList<>(franchise.getBranches());
+        updateBranches.add(buildBranch(name));
+        franchise.setBranches(updateBranches);
+        return Mono.just(franchise);
+    }
+
+    private Branch buildBranch(String name){
+        return Branch.builder()
+                .id(generateId())
+                .name(name)
+                .build();
     }
 }
