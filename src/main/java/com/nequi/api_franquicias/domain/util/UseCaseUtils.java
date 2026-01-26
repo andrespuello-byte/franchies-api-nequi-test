2929a02e84fb96cb23b786d8a55748930249b466
Andres Puello Osorio <andres.puello@pragma.com.com>
2026-01-26 14:33:36 -0500

    feat: add use case util file for duplicate functions in domain

---
+++ src/main/java/com/nequi/api_franquicias/domain/util/UseCaseUtils.java
@@ -0,0 +1,43 @@
+package com.nequi.api_franquicias.domain.util;
+
+import com.nequi.api_franquicias.domain.exceptions.BussinesException;
+import com.nequi.api_franquicias.domain.exceptions.ErrorMessage;
+import com.nequi.api_franquicias.domain.model.Branch;
+import com.nequi.api_franquicias.domain.model.Franchise;
+import com.nequi.api_franquicias.domain.model.Product;
+import com.nequi.api_franquicias.domain.ports.out.FranchisePersistencePort;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Supplier;
+
+public class UseCaseUtils {
+    public static boolean isInvalidName(String name){
+        return name.isEmpty();
+    }
+
+    public static Branch buildBranch(String name, Supplier<String> idGenerator){
+        return Branch.builder()
+                .id(idGenerator.get())
+                .name(name)
+                .build();
+    }
+
+    public static Product buildProduct(Product product, Supplier<String> idGenerator){
+        product.setId(idGenerator.get());
+        return product;
+    }
+
+    public static Branch findBranchById(Franchise franchise, String branchId){
+        return franchise.getBranches()
+                .stream()
+                .filter(branch -> branch.getId().equals(branchId))
+                .findFirst()
+                .orElseThrow(() -> new BussinesException(ErrorMessage.BRANCH_NOT_FOUND));
+    }
+
+    public static Mono<Franchise> findFranchiseById(FranchisePersistencePort persistencePort, String franchiseId){
+        return persistencePort
+                .findById(franchiseId)
+                .switchIfEmpty(Mono.error(new BussinesException(ErrorMessage.FRANCHISE_NOT_FOUND)));
+    }
+}
