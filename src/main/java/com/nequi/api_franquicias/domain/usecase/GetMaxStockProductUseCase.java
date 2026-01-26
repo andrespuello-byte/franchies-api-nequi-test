351f9e88580e0c92630e7dad6b084acf93af6b0d
Andres Puello Osorio <andres.puello@pragma.com.com>
2026-01-26 14:55:51 -0500

    reafactor: remove duplicate method in GetMaxStockProductUseCase

--- src/main/java/com/nequi/api_franquicias/domain/usecase/GetMaxStockProductUseCase.java
+++ src/main/java/com/nequi/api_franquicias/domain/usecase/GetMaxStockProductUseCase.java
@@ -1,16 +1,15 @@
 package com.nequi.api_franquicias.domain.usecase;
 
-import com.nequi.api_franquicias.domain.exceptions.BussinesException;
-import com.nequi.api_franquicias.domain.exceptions.ErrorMessage;
 import com.nequi.api_franquicias.domain.model.Branch;
 import com.nequi.api_franquicias.domain.model.Product;
 import com.nequi.api_franquicias.domain.ports.out.FranchisePersistencePort;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
 
 import java.util.Comparator;
+
+import static com.nequi.api_franquicias.domain.util.UseCaseUtils.findFranchiseById;
 
 public class GetMaxStockProductUseCase {
     private final FranchisePersistencePort persistencePort;
@@ -20,8 +19,7 @@ public class GetMaxStockProductUseCase {
     }
 
     public Flux<Tuple2<Branch, Product>> execute(String franchiseId){
-        return persistencePort.findById(franchiseId)
-                .switchIfEmpty(Mono.error(new BussinesException(ErrorMessage.FRANCHISE_NOT_FOUND)))
+        return findFranchiseById(persistencePort, franchiseId)
                 .flatMapMany(franchise ->
                         Flux.fromIterable(franchise.getBranches())
                                 .flatMap(branch ->
