41d3bbed1f783d3012be2427603dbea3d26e6b70
Andres Puello Osorio <andres.puello@pragma.com.com>
2026-01-27 10:01:03 -0500

    feat: modify model file and create use case for update branch name

---
+++ src/main/java/com/nequi/api_franquicias/domain/usecase/UpdateBranchNameUseCase.java
@@ -0,0 +1,38 @@
+package com.nequi.api_franquicias.domain.usecase;
+
+import com.nequi.api_franquicias.domain.model.Branch;
+import com.nequi.api_franquicias.domain.model.Franchise;
+import com.nequi.api_franquicias.domain.ports.out.FranchisePersistencePort;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+import static com.nequi.api_franquicias.domain.util.UseCaseUtils.findFranchiseById;
+import static com.nequi.api_franquicias.domain.util.UseCaseUtils.findBranchById;
+
+public class UpdateBranchNameUseCase {
+    private final FranchisePersistencePort persistencePort;
+
+    public UpdateBranchNameUseCase(FranchisePersistencePort persistencePort) {
+        this.persistencePort = persistencePort;
+    }
+
+    public Mono<Franchise> execute(String franchiseId, String branchId, String name){
+        return findFranchiseById(persistencePort, franchiseId)
+                .flatMap(franchise -> {
+                    Branch branchFound = findBranchById(franchise, branchId);
+
+                    Branch updatedBranch = branchFound.toBuilder()
+                            .name(name)
+                            .build();
+
+                    List<Branch> updatedBranches = franchise.getBranches().stream()
+                            .map(b -> b.getId().equals(branchId) ? updatedBranch : b)
+                            .toList();
+
+                    return persistencePort.save(franchise.toBuilder()
+                            .branches(updatedBranches)
+                            .build());
+                });
+    }
+}
