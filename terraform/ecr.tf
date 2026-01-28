1519861c4d913f0d63408e371a56efd53c513461
Andres Puello Osorio <andres.puello@pragma.com.com>
2026-01-28 01:43:14 -0500

    feat: add github deploy action file

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,4 +1,22 @@
 resource "aws_ecr_repository" "api" {
-  name = var.project_name
+  name         = var.project_name
   force_delete = true
+}
+
+resource "aws_ecr_repository_policy" "api_policy" {
+  repository = aws_ecr_repository.api.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "FullAccessForUser"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::008857349951:user/andres"
+        }
+        Action = "ecr:*"
+      }
+    ]
+  })
 }
